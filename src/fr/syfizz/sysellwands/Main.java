31a95d66473f0c85760d2c463061be0ff4e6a99a
SyFizz <gbertini.lpo@gmail.com>
2021-04-20 23:51:05 +0200

    Initial commit

---
+++ src/fr/syfizz/sysellwands/Main.java
@@ -0,0 +1,19 @@
+package fr.syfizz.sysellwands;
+
+import fr.syfizz.sysellwands.commands.SellWandsCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+        saveDefaultConfig();
+        System.out.println(this.getConfig().getString("messages.pluginEnabled").replaceAll("&","§"));
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+        this.getCommand("sellwands").setExecutor(new SellWandsCommand(this));
+    }
+    public void onDisable(){
+        System.out.println(getConfig().getString("messages.pluginDisabled").replaceAll("&","§"));
+    }
+
+}
